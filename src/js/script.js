d05315222e1f5b77b1705d57439176f3422f5c22
PabloGonzalez12 <115100458+PabloGonzalez12@users.noreply.github.com>
2024-09-24 18:16:31 +0200

    v1

---
+++ src/js/script.js
@@ -0,0 +1,75 @@
+const headElement = document.createElement("p");
+const divElement = document.createElement("div");
+const timeElement = document.createElement("span");
+const inputElement = document.createElement("input");
+
+const bodyElement = document.getElementById("body");
+bodyElement.appendChild(headElement);
+headElement.innerHTML = "Tiempo restante hasta las vacaciones de Navidad";
+bodyElement.appendChild(divElement);
+divElement.appendChild(timeElement);
+
+inputElement.setAttribute("type", "date");
+bodyElement.appendChild(inputElement);
+
+headElement.classList.add("head");
+divElement.classList.add("container");
+timeElement.classList.add("time");
+
+let date = new Date(2024, 11, 20, 21, 45);
+let countDown = new Date(date).getTime();
+
+
+let today = new Date();
+let y = today.getFullYear();
+let m = today.getMonth();
+let d = today.getDay();
+let todayDate = y + "-" + m + "-" + d
+inputElement.setAttribute("min", todayDate);
+
+
+
+inputElement.addEventListener("change", function() {
+    let selectedDate = inputElement.value;
+    if (selectedDate) {
+        countDown = new Date(selectedDate).getTime();
+
+        headElement.innerHTML = "¡¡ Tiempo restante !!";
+        divElement.classList.remove("green");
+        divElement.classList.remove("red");
+        divElement.classList.remove("orange");
+
+        count();
+    }
+});
+
+
+
+
+var count = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDown - now;
+        
+    var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30.44));
+    var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 30.44)) / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    timeElement.innerHTML = months + "m " + days + "d " + hours + "h " 
+    + minutes + "m " + seconds + "s ";
+
+    
+    if (days < 7 && months == 0){
+        divElement.classList.add("red");
+    } else if (days < 14 && months == 0){
+        divElement.classList.add("orange");
+    } else {
+        divElement.classList.add("green");
+    }
+        
+    if (seconds < 0) {
+        clearInterval(count);
+        timeElement.innerHTML = "Contador acabado";
+    }
+}, 1000);
